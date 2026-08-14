69a50793c39ca3783a516432b8714b5645e97b43
wiking21 <lowtiergod555@gmail.com>
2026-08-14 21:23:54 +0200

    Update wishlist items labels and remove icons

--- wishlist-config.js
+++ wishlist-config.js
@@ -1,12 +1,10 @@
 const WISHLIST_ITEMS = [
   {
-    label: "The Art of Gravity Falls",
-    url: "https://books.disney.com/book/the-art-of-gravity-falls/",
-    icon: "📖"
+    label: "gravity falls book",
+    url: "https://books.disney.com/book/the-art-of-gravity-falls/"
   },
   {
-    label: "Fender Squier Mini Stratocaster",
-    url: "https://www.amazon.de/Fender-Squier-Mini-Stratocaster-Dakota/dp/B085G462HT",
-    icon: "🎸"
+    label: "guitar",
+    url: "https://www.amazon.de/Fender-Squier-Mini-Stratocaster-Dakota/dp/B085G462HT"
   }
 ];
