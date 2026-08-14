f1362a5d984909995d134fcc12f8ccbae80d4892
wiking21 <lowtiergod555@gmail.com>
2026-08-14 21:17:39 +0200

    Add wishlist items configuration

---
+++ wishlist-config.js
@@ -0,0 +1,12 @@
+const WISHLIST_ITEMS = [
+  {
+    label: "The Art of Gravity Falls",
+    url: "https://books.disney.com/book/the-art-of-gravity-falls/",
+    icon: "📖"
+  },
+  {
+    label: "Fender Squier Mini Stratocaster",
+    url: "https://www.amazon.de/Fender-Squier-Mini-Stratocaster-Dakota/dp/B085G462HT",
+    icon: "🎸"
+  }
+];
